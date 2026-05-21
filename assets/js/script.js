f38a5885e5b3635c1b98ac9ad6f25ceac0ef2622
“Rosalinda <saputri.1124020015@students.aknpacitan.ac.id>
2026-05-21 15:58:50 +0700

    update script.js

--- assets/js/script.js
+++ assets/js/script.js
@@ -50,3 +50,35 @@ document.querySelectorAll(".nav-links a").forEach(link => {
     });
 
 });
+
+/* ACTIVE NAVBAR SAAT SCROLL */
+
+const sections = document.querySelectorAll("section, footer");
+const navLinks = document.querySelectorAll(".nav-links a");
+
+window.addEventListener("scroll", () => {
+
+    let current = "";
+
+    sections.forEach(section => {
+
+        const sectionTop = section.offsetTop - 120;
+        const sectionHeight = section.clientHeight;
+
+        if(window.scrollY >= sectionTop){
+            current = section.getAttribute("id");
+        }
+
+    });
+
+    navLinks.forEach(link => {
+
+        link.classList.remove("active");
+
+        if(link.getAttribute("href") === `#${current}`){
+            link.classList.add("active");
+        }
+
+    });
+
+});
